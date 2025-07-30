801f0e8d41afc6d977387c7f0376bc74717aed22
tiarnanapower <tiarnan.power@bigcommerce.com>
2025-07-30 14:21:36 +0100

    updating script

--- core/b2b/script-production.tsx
+++ core/b2b/script-production.tsx
@@ -18,63 +18,43 @@ export function ScriptProduction({ cartId, storeHash, channelId, token, environm
   useB2BCart(cartId);
 
     return (
-    // <>
-    //   <script>
-    //     {`
-    //       window.b3CheckoutConfig = {
-    //         routes: {
-    //           dashboard: '/account.php?action=order_status',
-    //         },
-    //       }
-    //       window.B3 = {
-    //         setting: {
-    //           store_hash: '${storeHash}',  
-    //           channel_id: ${channelId},
-    //           platform: 'catalyst',
-    //         },
-    //         'dom.checkoutRegisterParentElement': '#checkout-app',
-    //         'dom.registerElement': '[href^="/login.php"], #checkout-customer-login, [href="/login.php"] .navUser-item-loginLabel, #checkout-customer-returning .form-legend-container [href="#"]',
-    //         'dom.openB3Checkout': 'checkout-customer-continue',
-    //         before_login_goto_page: '/account.php?action=order_status',
-    //         checkout_super_clear_session: 'true',
-    //         'dom.navUserLoginElement': '.navUser-item.navUser-item--account',
-    //       }
-    //     `}
-    //   </script>
-    //   <script
-    //     type="module"
-    //     crossOrigin=''
-    //     src="https://demostoreb2b.netlify.app/index.C-1RMyzg.js"
-    //   ></script>
-    //   <script
-    //     crossOrigin=''
-    //     src="https://demostoreb2b.netlify.app/polyfills-legacy.D32xEujE.js"
-    //   ></script>
-    //   <script
-    //     crossOrigin=''
-    //     src="https://demostoreb2b.netlify.app/index-legacy.I7pICwj-.js"
-    //   ></script>
-      
-    // </>
-     <>
-      <Script id="b2b-config">
+    <>
+      <script>
         {`
-            window.B3 = {
-              setting: {
-                store_hash: '${storeHash}',
-                channel_id: ${channelId},
-                platform: 'catalyst',
-                cart_url: '/cart',
-              }
-            }
+          window.b3CheckoutConfig = {
+            routes: {
+              dashboard: '/account.php?action=order_status',
+            },
+          }
+          window.B3 = {
+            setting: {
+              store_hash: '${storeHash}',  
+              channel_id: ${channelId},
+              platform: 'catalyst',
+            },
+            'dom.checkoutRegisterParentElement': '#checkout-app',
+            'dom.registerElement': '[href^="/login.php"], #checkout-customer-login, [href="/login.php"] .navUser-item-loginLabel, #checkout-customer-returning .form-legend-container [href="#"]',
+            'dom.openB3Checkout': 'checkout-customer-continue',
+            before_login_goto_page: '/account.php?action=order_status',
+            checkout_super_clear_session: 'true',
+            'dom.navUserLoginElement': '.navUser-item.navUser-item--account',
+          }
         `}
-      </Script>
-      <Script
-        data-channelid={channelId}
-        data-storehash={storeHash}
-        src="https://demostoreb2b.netlify.app/headless.js"
+      </script>
+      <script
         type="module"
-      />
+        crossOrigin=''
+        src="https://demostoreb2b.netlify.app/index.C-1RMyzg.js"
+      ></script>
+      <script
+        crossOrigin=''
+        src="https://demostoreb2b.netlify.app/polyfills-legacy.D32xEujE.js"
+      ></script>
+      <script
+        crossOrigin=''
+        src="https://demostoreb2b.netlify.app/index-legacy.I7pICwj-.js"
+      ></script>
+      
     </>
   );
 }
