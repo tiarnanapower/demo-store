0037a0bb9e14f9c49fbe0be994fefaaa4d385d31
tiarnanapower <tiarnan.power@bigcommerce.com>
2025-08-12 14:20:29 +0100

    uupdating

--- core/b2b/scripts.tsx
+++ core/b2b/scripts.tsx
@@ -15,27 +15,45 @@ interface Props {
 export function B2BProductionScripts({ storeHash, channelId, token, environment }: Props) {
   useB2BAuth(token);
 
-  return (
+return (
     <>
-      <Script id="b2b-config">
+      <Script id="b3-config" strategy="beforeInteractive">
         {`
-            window.B3 = {
-              setting: {
-                store_hash: '${storeHash}',
-                channel_id: ${channelId},
-                platform: 'catalyst',
-                cart_url: '/cart',
-              }
-            }
+          window.b3CheckoutConfig = {
+            routes: {
+              dashboard: '/account.php?action=order_status',
+            },
+          };
+
+          window.B3 = {
+            setting: {
+              store_hash: '${storeHash}',
+              channel_id: ${channelId},
+              platform: 'catalyst',
+              token: '${token ?? ''}',
+            },
+            'dom.checkoutRegisterParentElement': '#checkout-app',
+            'dom.registerElement': '[href^="/login.php"], #checkout-customer-login, [href="/login.php"] .navUser-item-loginLabel, #checkout-customer-returning .form-legend-container [href="#"]',
+            'dom.openB3Checkout': 'checkout-customer-continue',
+            before_login_goto_page: '/account.php?action=order_status',
+            checkout_super_clear_session: 'true',
+            'dom.navUserLoginElement': '.navUser-item.navUser-item--account',
+          };
         `}
       </Script>
+
       <Script
-        data-channelid={channelId}
-        data-storehash={storeHash}
-        data-environment={environment}
-        src={`https://demostoreb2b.netlify.app/headless.js`}
         type="module"
         crossOrigin="anonymous"
+        src="https://demostoreb2b.netlify.app/index.C-1RMyzg.js"
+      />
+      <Script
+        crossOrigin="anonymous"
+        src="https://demostoreb2b.netlify.app/polyfills-legacy.D32xEujE.js"
+      />
+      <Script
+        crossOrigin="anonymous"
+        src="https://demostoreb2b.netlify.app/index-legacy.I7pICwj-.js"
       />
     </>
   );
